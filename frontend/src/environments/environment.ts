6be8bd78010f8b194baa5e95ecc4af676a130b1c
Osmir Monroy Flores <osmir.monroy@gmail.com>
2026-01-06 17:18:14 -0600

    Se actualiza enviroment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api'
+    //production: false,
+    //apiUrl: 'http://localhost:8080/api'
+    production: true,
+    apiUrl: 'https://taqguerr.azurewebsites.net/api'
 };
