540a80f36ebcd55b88d850d4a60b28579b989f87
Osmir Monroy Flores <osmir.monroy@gmail.com>
2026-02-23 18:33:27 -0600

    Se agrega mayor control al spinner

--- frontend/src/app/interceptors/loading.interceptor.ts
+++ frontend/src/app/interceptors/loading.interceptor.ts
@@ -11,24 +11,14 @@ import { LoadingService } from '../services/loading.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-    private activeRequests = 0;
-
     constructor(private loadingService: LoadingService) { }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        // Incrementar contador y mostrar spinner si es la primera solicitud
-        if (this.activeRequests === 0) {
-            this.loadingService.show();
-        }
-        this.activeRequests++;
+        this.loadingService.show();
 
         return next.handle(request).pipe(
             finalize(() => {
-                // Decrementar contador y ocultar spinner si no hay solicitudes activas
-                this.activeRequests--;
-                if (this.activeRequests === 0) {
-                    this.loadingService.hide();
-                }
+                this.loadingService.hide();
             })
         );
     }
--- frontend/src/app/services/loading.service.ts
+++ frontend/src/app/services/loading.service.ts
@@ -7,12 +7,46 @@ import { BehaviorSubject } from 'rxjs';
 export class LoadingService {
     private loadingSubject = new BehaviorSubject<boolean>(false);
     public loading$ = this.loadingSubject.asObservable();
+    private activeRequests = 0;
+    private safetyTimer: any;
 
     show() {
-        this.loadingSubject.next(true);
+        this.activeRequests++;
+        if (this.activeRequests === 1) {
+            this.loadingSubject.next(true);
+            this.startSafetyTimer();
+        }
     }
 
     hide() {
+        this.activeRequests--;
+        if (this.activeRequests <= 0) {
+            this.activeRequests = 0;
+            this.forceHide();
+        }
+    }
+
+    private forceHide() {
         this.loadingSubject.next(false);
+        this.clearSafetyTimer();
+    }
+
+    private startSafetyTimer() {
+        this.clearSafetyTimer();
+        // Safety: Hide spinner after 15 seconds even if requests are "pending"
+        this.safetyTimer = setTimeout(() => {
+            if (this.activeRequests > 0) {
+                console.warn('Loading safety timer triggered. Forcing hide.');
+                this.activeRequests = 0;
+                this.forceHide();
+            }
+        }, 15000);
+    }
+
+    private clearSafetyTimer() {
+        if (this.safetyTimer) {
+            clearTimeout(this.safetyTimer);
+            this.safetyTimer = null;
+        }
     }
 }
